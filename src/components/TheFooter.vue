2e01dcd5fe223f8c148f0b27cdcdc03db6fe6f01
Jhongxu <48619747+jhongxu@users.noreply.github.com>
2023-09-18 05:54:04 +0000

    feat: 添加图片拼接功能

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -22,16 +22,8 @@ async function toggleLocales() {
       <div i="carbon-sun dark:carbon-moon" />
     </button>
 
-    <a icon-btn :title="t('button.toggle_langs')" @click="toggleLocales()">
-      <div i-carbon-language />
-    </a>
-
     <RouterLink icon-btn to="/about" :title="t('button.about')" data-test-id="about">
       <div i-carbon-dicom-overlay />
     </RouterLink>
-
-    <a icon-btn rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank" title="GitHub">
-      <div i-carbon-logo-github />
-    </a>
   </nav>
 </template>
